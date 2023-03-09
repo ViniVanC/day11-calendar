1ecfad2c1d461be8b248ed694d527aeb799996cf
ViniVanC <vincentvan0302@gmail.com>
2023-03-09 15:43:03 +0200

    added component Container to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Container } from "./components/Container";
+import { useCalendar } from "./hook/useCalendar";
+
 function App() {
+  const { day, monthName, monthDays, year } = useCalendar();
+  console.log(day, monthName, monthDays, year);
   return (
-    <div className="flex h-[100vh] items-center justify-center text-[400px] font-black">
-      App
+    <div className="flex h-[100vh] items-center justify-center overflow-x-hidden bg-gradient-yellowPink">
+      <Container />
     </div>
   );
 }
