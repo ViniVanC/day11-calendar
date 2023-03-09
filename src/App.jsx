78603abbf9cdb8e23bed3ef882ca48580b721ea5
ViniVanC <vincentvan0302@gmail.com>
2023-03-09 11:26:30 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="flex h-[100vh] items-center justify-center text-[400px] font-black">
+      App
+    </div>
+  );
 }
 
 export default App;
