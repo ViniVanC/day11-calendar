6e68df06f6cec88d3afe0010e3c858425770d550
ViniVanC <vincentvan0302@gmail.com>
2023-03-09 16:04:09 +0200

    bug-fix

--- src/hook/useDB.jsx
+++ src/hook/useDB.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
+import { v4 } from "uuid";
 import { useCalendar } from "./useCalendar";
 
 export const useDB = () => {
-  const [daysList, setDaysList] = useState();
+  const [daysList, setDaysList] = useState([]);
   const { monthDays } = useCalendar();
 
   // створюю масив з днями
@@ -10,11 +11,11 @@ export const useDB = () => {
     let arr = [];
 
     for (let i = 1; i <= monthDays; i++) {
-      arr.push({ dayNumber: i });
+      arr.push({ id: v4(), dayNumber: i });
     }
 
     setDaysList(arr);
-  }, []);
+  }, [monthDays]);
 
   return { daysList };
 };
