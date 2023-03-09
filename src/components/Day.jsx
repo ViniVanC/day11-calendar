7fe36be1e18872f48847d46f444dd9f44e47ec46
ViniVanC <vincentvan0302@gmail.com>
2023-03-09 17:41:59 +0200

    commit

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -4,9 +4,13 @@ export const Day = ({ dayNumber, dayNow }) => {
   return (
     <div
       className={`flex h-20 w-20 items-end justify-end rounded-xl p-2 font-BungeeShade text-[30px] text-green ${
-        dayNow ? "bg-green text-black" : "bg-black"
+        dayNow ? "bg-green text-black " : "bg-black"
       }`}
-      style={{ boxShadow: "5px 5px 10px #222b2f, -5px -5px 10px #324147" }}
+      style={{
+        boxShadow: dayNow
+          ? "none"
+          : "5px 5px 10px #222b2f, -5px -5px 10px #324147",
+      }}
     >
       {dayNumber}
     </div>
