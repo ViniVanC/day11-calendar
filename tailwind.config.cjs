fe1b7211f639516a4add22841fb605d4960098c1
ViniVanC <vincentvan0302@gmail.com>
2023-03-09 14:44:46 +0200

    set up tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,30 +4,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "gray-20": "#f8f4eb",
-        "gray-50": "#efe6e6",
-        "gray-100": "#dfcccc",
-        "gray-500": "#5e0000",
-        "primary-100": "#ffe1e0",
-        "primary-300": "#ffa6a3",
-        "primary-500": "#ff6b66",
-        "secondary-400": "#ffcd58",
-        "secondary-500": "#ffc132",
+        green: "#99b898",
+        yellow: "#feceab",
+        lightPink: "#ff847c",
+        pink: "#e84a5f",
+        black: "#2a363b",
       },
       backgroundImage: (theme) => ({
-        "gradient-yellowRed":
-          "linear-gradient(90deg, #ff616a 0%, #ffc837 100%)",
-        "mobile-home": "url('./assets/HomePageGraphic.png')",
+        "gradient-yellowPink":
+          "linear-gradient(0deg, rgba(254,206,171,1) 0%, rgba(255,132,124,1) 100%)",
       }),
       fontFamily: {
-        DMSans: ["DM Sans", "sans-serif"],
+        BungeeShade: ["Bungee Shade", "cursive"],
         Montserrat: ["Montserrat", "sans-serif"],
-      },
-      content: {
-        evolveText: "url('./assets/EvolveText.png')",
-        abstractWaves: "url('./assets/AbstractWaves.png')",
-        sparkles: "url('./assets/Sparkles.png')",
-        circles: "url('./assets/Circles.png')",
       },
     },
     screens: {
